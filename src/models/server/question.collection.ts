44fc836643c93674a69f9921142751f6bd087426
adesh003 <theadeshkumar03@gmail.com>
2025-10-05 00:24:48 +0530

    attribute_created

--- src/models/server/question.collection.ts
+++ src/models/server/question.collection.ts
@@ -1,15 +1,46 @@
-import {Permission} from 'node-appwrite'
+import { IndexType, Permission } from 'node-appwrite'
 
-import {db, questionCollection} from "../name"
+import { db, questionCollection } from "../name"
 import { databases } from './config'
 
-export default async function createQuestionCollection (){
-    await databases.createCollection(db , questionCollection,questionCollection, [
+export default async function createQuestionCollection() {
+    await databases.createCollection(db, questionCollection, questionCollection, [
         Permission.read('any'),
         Permission.read('users'),
         Permission.create('users'),
         Permission.update('users'),
         Permission.delete('users'),
     ])
+
     console.log("question collection created ")
+    // creating attribute and index
+
+    await Promise.all([
+        databases.createStringAttribute(db, questionCollection, "title", 100, true),
+        databases.createStringAttribute(db, questionCollection, "content", 10000, true),
+        databases.createStringAttribute(db, questionCollection, "authorId", 50, true),
+        databases.createStringAttribute(db, questionCollection, "tages", 50, true),
+        databases.createStringAttribute(db, questionCollection, "attachmentId", 50, false),
+
+    ])
+    console.log("attributed created ")
+
+    //create index
+
+    await Promise.all([
+        databases.createIndex(
+            db, questionCollection, "title", IndexType.Fulltext,
+            ["title"],
+            ['asc']
+        ),
+        databases.createIndex(
+            db, 
+            questionCollection,
+            "content", 
+            IndexType.Fulltext,
+            ["content"],
+            ['asc']
+        )
+    ])
+
 }
